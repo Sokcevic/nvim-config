bdf0afaf6560656e4254a33c2d259464e667947e
Lukas Sokcevic <lukas.sokcevic@isys.de>
2024-06-13 18:34:03 +0200

    remap exit terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("t", "<Esc>", "<C-\\><C-N>", {desc = "Exit insert Mode in Terminal"})
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
