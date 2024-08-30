4b33e5701b6d2c6c48f381eb141cb98050a9c25b
Lukas Sokcevic <lukas.sokcevic@isys.de>
2024-08-30 11:48:36 +0200

    lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 require("mason").setup()
 require("mason-lspconfig").setup {
-  ensure_installed = {  "html", "cssls", "dartls", "jdtls", "clangd", "pylsp", "java-debug"}
+  ensure_installed = {  "html", "cssls", "dartls", "jdtls", "clangd", "pylsp", "java-debug-adapter"}
 }
 
 local lspconfig = require "lspconfig"
