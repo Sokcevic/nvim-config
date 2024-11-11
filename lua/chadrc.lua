a9442aeebefd1b479fa456b5b2c1d1dcef7cf240
Lukas Sokcevic <lukas.sokcevic@isys.de>
2024-11-11 18:19:59 +0100

    LSP_Signature

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,26 +2,27 @@
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
-local M = {}
+local M = {
+  base46 = {
+    theme = "bearded-arc",
+    pkgs = {
+      "lua-language-server",
+      "stylua",
+      "html-lsp",
+      "css-lsp",
+      "prettier",
+      "jdtls",
+      "clangd",
+      "pyright",
+      "java-debug-adapter",
+    },
 
-M.base46 = {
-  theme = "bearded-arc",
-  pkgs = {
-    "lua-language-server",
-    "stylua",
-    "html-lsp",
-    "css-lsp",
-    "prettier",
-    "jdtls",
-    "clangd",
-    "pyright",
-    "java-debug-adapter",
+    -- hl_override = {
+    -- 	Comment = { italic = true },
+    -- 	["@comment"] = { italic = true },
+    -- },
   },
-
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+  lsp = { signature = false },
 }
 
 return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,11 +5,9 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-map("i", "jk", "<ESC>")
-map("t", "<Esc>", "<C-\\><C-N>", {desc = "Exit insert Mode in Terminal"})
-map("n", "<leader>dui", "<Cmd>lua require('dapui').toggle()<CR>", {desc = "Debugger Toggle UI"})
-map("n", "<leader>db", "<Cmd>DapToggleBreakpoint<CR>", {desc = "Debugger Toggle Breakpoint"})
-map("n", "<leader>dr", "<Cmd>DapContinue<CR>", {desc = "Debugger Run Programm"})
+map("t", "<Esc>", "<C-\\><C-N>", { desc = "Exit insert Mode in Terminal" })
+map("n", "<leader>dui", "<Cmd>lua require('dapui').toggle()<CR>", { desc = "Debugger Toggle UI" })
+map("n", "<leader>db", "<Cmd>DapToggleBreakpoint<CR>", { desc = "Debugger Toggle Breakpoint" })
+map("n", "<leader>dr", "<Cmd>DapContinue<CR>", { desc = "Debugger Run Programm" })
 map("", "<Leader>dl", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
